c8978663ff776233b04788ba3af21edebd979a5e
devilangelv2 <devilangelv1@gmail.com>
2016-01-06 19:41:50 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -227,7 +227,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {139837032},
+    sudo_users = {139837032,70742427},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
